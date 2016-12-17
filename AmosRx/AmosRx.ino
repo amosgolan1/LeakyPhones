a37728c88be33c1a03fd783b62d02ea662af3c66
amosgolan1 <amosgolan1@gmail.com>
2016-12-16 20:09:17 -0500

    first working version

--- AmosRx/AmosRx.ino
+++ AmosRx/AmosRx.ino
@@ -11,12 +11,30 @@ int SCLK = A5;
 
 #define DEFAULT_STATION 9290
 #define DEFAULT_CHANNEL freqToChannel(DEFAULT_STATION)
-#define MILLIS_BEFORE_RETURN_TO_DEFUALT 1400
+
+#define TIME_PER_IR_SEND 108
+#define MILLIS_BEFORE_RETURN_TO_DEFUALT 3*TIME_PER_IR_SEND
 
 Si4703_Breakout radio(resetPin, SDIO, SCLK);
-int radiochannel;
-int volume;
-long lastValidReceived;
+int radioChannel;
+int radioVolume;
+
+int16_t lastValidAmos_Code;
+long lastValidAmos_Time;
+
+
+//utility variables for changing radio channels
+int rcChange_DestChannel;
+long rcChange_Time;
+
+//utility variables for changing volume
+long volChange_StartTime;
+long volChange_DurationMs;
+int volChange_StartVol;
+int volChange_EndVol;
+
+#define NEXTCHANNEL_NONE 0
+int nextChannel = NEXTCHANNEL_NONE;
 
 /*
  * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
@@ -39,8 +57,18 @@ void setup()
   Serial.begin(9600);
   irrecv.enableIRIn(); // Start the receiver
 
+  Serial.println("AMOS FM RX V0.2");
+
+//  rcChange_DestChannel;
+//  rcChange_Time;
+
+  volChange_StartTime = 0;
+  volChange_DurationMs = 0;
+  volChange_StartVol = 0;
+  volChange_EndVol = 7;
+
   radio.powerOn();
-  radio.setVolume(7);
+  setVolumeIfNeeded(7);
 }
 
 
@@ -148,34 +176,129 @@ bool isValidAmos(int16_t amos) {
 }
 
 /**
- * retune SI4703 to given FCC channel, iff not already at that channel;
+ * schedule immediate tune of SI4703 to given FCC channel, iff not already at that channel;
  */
 void tuneIfNeeded(int16_t channel) {
   int16_t newRadioChannel = adafruitToSparkfun(channelToFreq(channel));
-  if (radiochannel != newRadioChannel) {
-    radiochannel = newRadioChannel;
-     radio.setChannel(radiochannel);
+  if (radioChannel != newRadioChannel) {
+    rcChange_DestChannel = newRadioChannel;
+    rcChange_Time = millis(); //now
   }
 }
 
+/**
+ * make sure we're tuned to the currently scheduled station, must be periodically called from the loop.
+ */
+void tuneIfScheduled() {
+  if (radioChannel != rcChange_DestChannel) {
+    //channel change is needed
+    if (millis() > rcChange_Time) {
+      //it's past the time for the channel change
+      radioChannel = rcChange_DestChannel;
+      radio.setChannel(radioChannel);
+    }
+  }
+}
+
+/**
+ * sets the radio volume to the given vol [0..15], and takes care of housekeeping of our internal variables
+ */
+void setVolumeIfNeeded(int vol) {
+  if (radioVolume != vol) {
+    radioVolume = vol;
+    radio.setVolume(radioVolume);
+  }
+}
+
+void setVolumeByTime() {
+  long currentTime = millis();
+  if (currentTime < volChange_StartTime) {
+    return; //nothing to do yet;
+  }
+
+  long endTime = volChange_StartTime + volChange_DurationMs; 
+  Serial.print("T:");
+  Serial.print(volChange_StartTime);
+  Serial.print("\t");
+  Serial.print(volChange_DurationMs);
+  Serial.print("\t");
+  Serial.print(endTime);
+  
+  if (currentTime >= endTime) {
+    //we're past the end time... make sure volume is set.
+    setVolumeIfNeeded(volChange_EndVol);
+    
+  } else {
+    
+    //we know we're after the start time and before the end time...
+    float newVolume = 
+        ((currentTime - volChange_StartTime)/(1.0f*volChange_DurationMs)) * volChange_EndVol
+        +
+        ((endTime - currentTime)/(1.0f*volChange_DurationMs)) * volChange_StartVol;
+    Serial.print("New Volume:");
+    Serial.print(newVolume);
+    setVolumeIfNeeded((int)newVolume);           
+  }
+
+}
+
 void printStatus() {
-  Serial.println("----");
-  Serial.print("Current radio channel: ");
-  Serial.println(radiochannel);
-  Serial.print("Time since last valid receive: ");
-  Serial.println(timeSinceLastValidReceive());
-  Serial.println("----");
+
+  Serial.print("nc:");
+  Serial.print(nextChannel);
+  Serial.print("rChan:");
+  Serial.print(radioChannel);
+  //Serial.print("\tLast received valid code: ");
+  //Serial.print(lastValidAmos_Code,HEX);
+  Serial.print("\t Vol:");
+  Serial.print(radioVolume);
+  Serial.print("\ttLVR:");
+  Serial.print(timeSinceLastValidReceive());
+
+  Serial.println();
+  //Serial.println("----");
 }
 
 long timeSinceLastValidReceive() {
-  return millis() - lastValidReceived;
+  return millis() - lastValidAmos_Time;
 }
 
-//void 
+/**
+ * set the channel to switch within given ms
+ */
+void setTuneFuture(int channel, long ms) {
+  rcChange_DestChannel = channel;
+  rcChange_Time = millis() + ms;
+}
+
+/**
+ * set the volume to fade to a certain level, with given pace in ms/step, starting immediate
+ */
+void setVolumeFuture(int vol, long msPerStep) {
+ volChange_StartTime = millis();
+ volChange_DurationMs = msPerStep * abs(radioVolume-vol);
+ volChange_StartVol = radioVolume;
+ volChange_EndVol = vol;
+}
+
 
 void loop() {
+
+  ////>>> HOUSEKEEPING -- MUST RUN FREQUENTLY
+  tuneIfScheduled();
+  setVolumeByTime();
   
-   
+  if (0==radioVolume && (nextChannel != NEXTCHANNEL_NONE)) {
+    Serial.println("RETUNING");
+    tuneIfNeeded(nextChannel);
+    setVolumeFuture(7,100);
+    
+    nextChannel = NEXTCHANNEL_NONE;
+  }
+  ///<<<<
+
+  static bool tunedToIR = false;
+     
   if (irrecv.decode(&results)) {
     //we receieved something
     if (results.decode_type == JVC) {
@@ -183,13 +306,27 @@ void loop() {
       
       //Serial.println(results.value, HEX);
       int16_t decodedAmos = amosToChannel(results.value);
-      Serial.println(decodedAmos, DEC);
+      //Serial.println(decodedAmos, DEC);
       if (decodedAmos != AMOS_INVALID) {
         //it is a valid amos code
-        tuneIfNeeded(decodedAmos);
+        if (!tunedToIR) {
+          tunedToIR = true;
+          
+          //tuneIfNeeded(decodedAmos);//setTuneFuture(decodedAmos,0);
+          //setVolumeFuture(7,333); 
+          Serial.println("Switching by IR code!");
+
+          if (radioChannel == adafruitToSparkfun(channelToFreq(decodedAmos))) {
+            setVolumeFuture(7,100);
+          } else {
+            nextChannel = decodedAmos;
+            setVolumeFuture(0,100);
+          }
+        }
 
         //record this moment as the last valid receive
-        lastValidReceived=millis();
+        lastValidAmos_Code=results.value;
+        lastValidAmos_Time=millis();
         
       }//if valid amos
        
@@ -198,12 +335,24 @@ void loop() {
     //dump(&results);
     irrecv.resume(); // Receive the next value
     
-  }// received some ir
+  } // received some ir
 
+  
   if ( timeSinceLastValidReceive() > MILLIS_BEFORE_RETURN_TO_DEFUALT) {
-    tuneIfNeeded(DEFAULT_CHANNEL);
+    //tuneIfNeeded(DEFAULT_CHANNEL);
+    if (tunedToIR) {
+      Serial.println("Switching by LACK OF IR code!");
+      tunedToIR = false;
+      
+      if (radioChannel == adafruitToSparkfun(channelToFreq(DEFAULT_CHANNEL))) {
+        setVolumeFuture(7,100);
+      } else {
+        nextChannel = DEFAULT_CHANNEL;
+        setVolumeFuture(0,100);
+      }
+    }
   }
 
   printStatus();
-  delay(500);
+  delay(50);
 }
