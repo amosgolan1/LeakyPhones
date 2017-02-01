dd2c72472443519e7cffe08abf046367cbf622da
amosgolan1 <amosgolan1@gmail.com>
2017-02-01 16:30:43 -0500

    debuting using signal counting
    counts 40 signals before it goes back to self frequency -1

--- IR_RX_with_mixing_new/IR_RX_with_mixing_new.ino
+++ IR_RX_with_mixing_new/IR_RX_with_mixing_new.ino
@@ -8,6 +8,9 @@ IRsend irsend;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
+//debug: 0 - nothing, 1 - numerical for graphing, 2 - full text
+#define DEBUG 1
+
 //variables to track and control mixing
 long duration = 2560;//mixing duration from volume 0 to volume 255
 int step_direction = 1;// used to change the mixing direction
@@ -35,6 +38,10 @@ int freq = 10300;
 //state No. and names
 const int RECEIVING = 0;
 const int MIX = 1;
+
+int noSignalCount = 0;
+#define HOW_MANY_SIGNALS_TO_MISS_BEFORE_MIXING_BACK_TO_SELF 40
+
 
 
 
@@ -93,7 +100,7 @@ void loop() {
 
   //new code
 
- 
+  static int receiveType = 0;
   switch (state)
   {
     case RECEIVING:
@@ -102,15 +109,17 @@ void loop() {
 
       // received valid ir?
       //yes
-      Serial.println("receiving");
+      if (DEBUG>=2) { Serial.println("receiving"); }
       if (irrecv.decode(&results) == true) {
-        Serial.println("waiting for signal");
+        if (DEBUG>=2) { Serial.println("waiting for signal"); }
         if (results.decode_type == JVC) {
-          Serial.println("received NEC code    ");
+          if (DEBUG>=2) { Serial.println("received NEC code    "); }
           int channel = results.value;
-          Serial.println(channel);
+          if (DEBUG>=2) { Serial.println(channel); }
           if (channel >= 0 && channel <= 100) {
-
+            noSignalCount = 0; //reset counter for 'mix back'
+            receiveType = 30;
+            
             ////addition
 //            time_from_last_change = millis() - last_change;
 //            if (time_from_last_change >= 30) {
@@ -118,10 +127,12 @@ void loop() {
               ///end addition
               
               current_frequancy = channelToFreq(channel);
-              Serial.print("frequancy:");
-              Serial.println(current_frequancy);
-              Serial.print("previous frequancy:");
-              Serial.println(previous_frequancy);
+              if (DEBUG>=2) {
+                Serial.print("frequancy:");
+                Serial.println(current_frequancy);
+                Serial.print("previous frequancy:");
+                Serial.println(previous_frequancy);
+              }
               //is current =previous?
               //yes
               if (current_frequancy == previous_frequancy) {
@@ -129,20 +140,26 @@ void loop() {
                 //am I at the maximum of channel 2
                 //yes
                 if (volume_channel_2 == 255) {
-                  Serial.println("ive reached the end of channel 2");
+                  if (DEBUG>=2) {
+                    Serial.println("ive reached the end of channel 2");
+                  }
                   state = RECEIVING;
                 }
                 //no
                 else {
                   //previous_frequancy = current_frequancy;
-                  Serial.println("stil didn't reach 255 in channel 2");
+                  if (DEBUG>=2) {
+                    Serial.println("stil didn't reach 255 in channel 2");
+                  }
                   step_direction = -1;
                   state = MIX;
                 }
               }
               //no, current!=previous
               else {
-                Serial.println("previous is -1, current is somthing new");
+                if (DEBUG>=2) {
+                  Serial.println("previous is -1, current is somthing new");
+                }
                 previous_frequancy = current_frequancy;
                 step_direction = -1;
                 state = MIX;
@@ -158,23 +175,29 @@ void loop() {
       //didn't receive anything
       //(irrecv.decode(&results)==false)
       else {
-      
+        receiveType = 50;
         time_from_last_change = millis() - last_change;
-        if (time_from_last_change >= 20) {
+        if (time_from_last_change >= 200) {
 
 
           
-          Serial.println("didn't receive anything, current frequancy=-1");
+          if (DEBUG>=2) {
+            Serial.println("didn't receive anything, current frequancy=-1");
+          }
           current_frequancy = -1;
           //is current =previous?
           //yes
           if (current_frequancy == previous_frequancy) {
-            Serial.println("previous frequancy= -1");
+            if (DEBUG>=2) {
+              Serial.println("previous frequancy= -1");
+            }
             previous_frequancy = current_frequancy;
             //am I at the maximum of channel 1?
             //yes
             if (volume_channel_1 == 255) {
-            Serial.println("reached 255 in channel 1");
+            if (DEBUG>=2) {
+              Serial.println("reached 255 in channel 1");
+            }
               state = RECEIVING;
               //break;
 
@@ -182,7 +205,9 @@ void loop() {
             //no
             else {
               step_direction = 1;
-              Serial.println("stil didn't reach 255 in channel 1");
+              if (DEBUG>=2) {
+                Serial.println("stil didn't reach 255 in channel 1");
+              }
               state = MIX;
 //              Serial.println("Not recievd, still mixing");
               //break;
@@ -190,18 +215,29 @@ void loop() {
           }
           //no
           else {
-          Serial.println("previous frequancy!=-1");
+            if (DEBUG>=2) {
+              Serial.println("previous frequancy!=-1");
+            }
             step_direction = 1;
-            previous_frequancy = current_frequancy;
-            state = MIX;
+            
+            if (noSignalCount > HOW_MANY_SIGNALS_TO_MISS_BEFORE_MIXING_BACK_TO_SELF) {
+              state = MIX;   
+              previous_frequancy = current_frequancy;         
+            } else {
+              noSignalCount++;
+            }
 
             //break;
 
           }
-          Serial.println("last change was more then 100ms ago");
+          if (DEBUG>=2) {
+            Serial.println("last change was more then 100ms ago");
+          }
         }
       }
-      Serial.println("finished the receiveing loop");
+      if (DEBUG>=2) {
+        Serial.println("finished the receiveing loop");
+      }
       //irrecv.resume(); // Receive the next value
 
       break;
@@ -209,7 +245,9 @@ void loop() {
 
 
     case MIX:
-      Serial.println("mixing");
+      if (DEBUG>=2) {
+        Serial.println("mixing");
+      }
       //record the current time
       long time_from_last_step = millis() - last_step;
       //is it time to do another mixing step?
@@ -219,12 +257,18 @@ void loop() {
         volume_channel_1 += step_direction * 5;
         volume_channel_2 -= step_direction * 5;
 
-        Serial.print("channel_1_volume is: ");
-        Serial.println(volume_channel_1);
-        Serial.print("channel_2_volume is: ");
-        Serial.println(volume_channel_2);
+        //to fix/prevent any over-range bugs:
+        volume_channel_1 = min(max(volume_channel_1,0),255);
+        volume_channel_2 = min(max(volume_channel_2,0),255);
+
+        if (DEBUG>=2) {
+          Serial.print("channel_1_volume is: "); 
+          Serial.println(volume_channel_1);
+          Serial.print("channel_2_volume is: ");
+          Serial.println(volume_channel_2);
         //Serial.println(previous_frequancy);
         //Serial.println(current_frequancy);
+        }
         last_change = millis();
         state = RECEIVING;
       }
@@ -234,5 +278,20 @@ void loop() {
       }
       break;
   }
+        if (DEBUG>=1) {
+          Serial.print(volume_channel_1);
+          Serial.print(" ");
+          Serial.print(volume_channel_2);  
+          if (MIX == state) {
+            Serial.print(" 200 ");   
+          } else {
+            Serial.print(" 100 ");
+          }
+          Serial.print(receiveType);
+          Serial.print(" ");
+          Serial.println(noSignalCount*10);             
+        }
+
+  delay(120); //TAL HACK: TEST DELAY EFFECT ON DECISION MAKING INSIDE STATE MACHINE
 }
 
